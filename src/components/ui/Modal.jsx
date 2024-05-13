7e8f60fc69fd6908bd2f1f10c6efe2ea2f9f5f24
Zak <zmooney@hotmail.co.uk>
2024-05-13 16:17:53 +0100

    chore: merge conflict

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -11,7 +11,27 @@ const Modal = (props) => {
     <>
       {open ? (
         <div
-          className="fixed z-50 top-0 left-0 right-0 bottom-0"
+          // className="fixed z-50 top-0 left-0 right-0 bottom-0"
+          style={{
+            position: 'fixed',
+            inset: '0px',
+            margin: '0px',
+            display: 'grid',
+            height: '100%',
+            maxHeight: 'none',
+            width: '100%',
+            maxWidth: 'none',
+            justifyItems: 'center',
+            padding: '0px',
+            overscrollBehavior: 'contain',
+            zIndex: '999',
+            backgroundColor: 'transparent',
+            color: 'inherit',
+            transitionDuration: '200ms',
+            transitionTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+            transitionProperty: 'transform, opacity, visibility',
+            overflowY: 'hidden',
+          }}
         >
           <div
             className={"z-40 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-full px-4 " + (wide ? "max-w-[48rem]" : "max-w-[34rem]") }
