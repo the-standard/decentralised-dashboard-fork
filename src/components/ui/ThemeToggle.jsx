7263633f77cddbc82f50735e705c09ce9a68b451
Zak <zmooney@hotmail.co.uk>
2024-05-24 12:22:43 +0100

    feat: add theme selector

--- src/components/ui/ThemeToggle.jsx
+++ src/components/ui/ThemeToggle.jsx
@@ -1,28 +1,42 @@
 import {
   useCurrentTheme,
 } from "../../store/Store";
-
 import Button from "./Button";
 
-import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
+import {
+  SunIcon,
+  MoonIcon,
+} from '@heroicons/react/24/solid';
 
-const ThemeToggle = () => {
+const ThemeToggle = (props) => {
+  const classes = props.className || '';
+  const buttonType = props.buttonTyle || 'square';
   const { setCurrentTheme } = useCurrentTheme();
 
   const chosenTheme = localStorage.getItem('theme');
 
-  const toggleTheme = () => {
-    switch (chosenTheme) {
-      case 'light':
-        saveTheme("dark");
-        break;
-      case 'dark':
-        saveTheme("light");
-        break;
-      default:
-        saveTheme("light");
-        break;
-    }  
+  // const toggleTheme = () => {
+  //   switch (chosenTheme) {
+  //     case 'deluxe-light':
+  //       saveTheme("deluxe-dark");
+  //       break;
+  //     case 'deluxe-dark':
+  //       saveTheme("deluxe-light");
+  //       break;
+  //     case 'nebula-light':
+  //       saveTheme("nebula-dark");
+  //       break;
+  //     case 'nebula-dark':
+  //       saveTheme("nebula-light");
+  //       break;  
+  //     default:
+  //       saveTheme("nebula-dark");
+  //       break;
+  //   }  
+  // }
+
+  const setTheme = (theme) => {
+    saveTheme(theme);
   }
 
   const saveTheme = (newTheme) => {
@@ -30,20 +44,81 @@ const ThemeToggle = () => {
     setCurrentTheme(newTheme);
   }
 
+  const isLight = chosenTheme.includes('light');
+
   return (
-    <Button
-      shape="circle"
-      color="ghost"
-      className="md:mr-2"
-      onClick={() => toggleTheme()}
+    <div
+      className={`dropdown ${classes ? classes : ''}`}
     >
-      {chosenTheme === 'light' ? (
-        <SunIcon className="w-6 h-6"/>
-      ) : (
-        <MoonIcon className="w-6 h-6"/>
-      )}
-    </Button>
-  );
+      <Button
+        tabIndex={0}
+        shape={buttonType}
+        color="ghost"
+        className="md:mr-2"
+      >
+        {isLight ? (
+          <SunIcon className="w-6 h-6"/>
+        ) : (
+          <MoonIcon className="w-6 h-6"/>
+        )}
+      </Button>
+      <div tabIndex={0} className="dropdown-content p-2 shadow-md rounded-box w-40 mt-4 tst-card opacity-100 before:backdrop-blur-[8px]">
+        <div className="relative flex flex-col">
+          <Button
+            color="ghost"
+            size="sm"
+            className="justify-between"
+            onClick={() => setTheme('deluxe-dark')} 
+          >
+            Deluxe Dark
+            <MoonIcon className="w-4 h-4"/>
+          </Button>
+          <Button
+            color="ghost"
+            size="sm"
+            className="justify-between"
+            onClick={() => setTheme('deluxe-light')} 
+          >
+            Deluxe Light
+            <SunIcon className="w-4 h-4"/>
+          </Button>
+          <Button
+            color="ghost"
+            size="sm"
+            className="justify-between"
+            onClick={() => setTheme('nebula-dark')} 
+          >
+            Nebula Dark
+            <MoonIcon className="w-4 h-4"/>
+          </Button>
+          <Button
+            color="ghost"
+            size="sm"
+            className="justify-between"
+            onClick={() => setTheme('nebula-light')} 
+          >
+            Nebula Light
+            <SunIcon className="w-4 h-4"/>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+
+  // return (
+    // <Button
+    //   shape="circle"
+    //   color="ghost"
+    //   className="md:mr-2"
+    //   onClick={() => toggleTheme()}
+    // >
+    //   {chosenTheme === 'light' ? (
+    //     <SunIcon className="w-6 h-6"/>
+    //   ) : (
+    //     <MoonIcon className="w-6 h-6"/>
+    //   )}
+    // </Button>
+  // );
 };
 
 export default ThemeToggle;
