2e4501ba18371f90c54ef7af633d83c122a91441
Zak <zmooney@hotmail.co.uk>
2024-12-23 13:10:11 +0000

    wip: add guest mode

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,27 +2,40 @@ import TSTIconSmallBG from "../assets/TSTIconSmallBG.png";
 
 import Card from "../components/ui/Card";
 import Typography from "../components/ui/Typography";
+import Button from "../components/ui/Button";
 
 import RainbowConnect from "../components/RainbowConnectButton";
 
 function Welcome(){
 
   return(
-    <div className="min-h-screen bg-base-300 flex items-center justify-center">
+    <div className="min-h-screen bg-base-300 flex items-center justify-center mx-4">
       <Card className="card-compact">
         <div className="card-body">
-          <div className="py-24 px-10">
+          <div className="py-4 md:py-20 px-2 md:px-8">
             <div className="flex justify-center content-center mb-3">
               <img src={TSTIconSmallBG} className="w-12 inline-block mask mask-circle mr-4" alt="logo" />
               <Typography variant="h1" className="text-3xl/loose text-center font-bold">
                 {import.meta.env.VITE_COMPANY_DAPP_NAME || ''}
               </Typography>
             </div>
-            <Typography variant="p" className="font-semibold text-center mb-8">
+            <Typography variant="p" className="font-semibold text-center mb-4">
               To start staking and earning with TST connect your wallet below.
             </Typography>
             <div className="flex justify-center content-center">
               <RainbowConnect />
+            </div>
+            <div class="divider py-3">OR</div>
+            <Typography variant="p" className="text-center mb-4">
+              Take a look around the dashboard below.
+            </Typography>
+            <div className="flex justify-center content-center">
+              <Button
+                className="w-full lg:w-auto"
+                variant="outline"
+              >
+                View Guest Showcase
+              </Button>
             </div>
           </div>
         </div>
--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -10,6 +10,14 @@ import liquidationPoolAbi from "../abis/liquidationPool";
 import stakingPoolv2Abi from "../abis/stakingPoolV2";
 import stakingPoolv3Abi from "../abis/stakingPoolV3";
 
+export const useGuestShowcaseStore = create(
+  (set) => ({
+    useShowcase: false,
+    showcaseWallet: '0xA409A235Cc15A5126Dd2a101b5460E713B90A7aF',
+    setUseShowcase: (useShowcase) => set(() => ({ useShowcase: useShowcase })),
+  })
+);
+
 export const useCurrentWagmiConfig = create(
   (set) => ({
     wagmiConfig: null,
